4f675d591ccc934a6b926bb97b35aa5146ac8169
BonorinoIgnacio <149428897+BonorinoIgnacio@users.noreply.github.com>
2024-04-09 14:29:02 -0300

    Add files via upload

---
+++ viaje.php
@@ -0,0 +1,57 @@
+<?php 
+class viaje{
+    private $codigo;
+    private $destino;
+    private $canMax;
+    private $cantPasajeros;
+    private $objPasajeros;//arreglo multidimencional de pasajeros
+
+public function __construct($cod,$destino,$cantMax,$array)
+{
+    $this->codigo=$cod;
+    $this->destino=$destino;
+    $this->canMax=$cantMax;
+   $this->cantPasajeros=count($array);
+} 
+//------------------------------Get
+public function getCodigo(){
+    return $this->codigo;
+}
+public function getDestino (){
+    return $this->destino;
+}
+public function getCantMax(){
+    return $this->canMax;
+}
+public function getCantPasajeros(){
+    return $this->cantPasajeros;
+}
+
+public function getPasajeros(){
+    return $this->objPasajeros;
+}
+//------------------------------Set
+public function setCodigo ($cod){
+    $this->codigo=$cod;
+}
+public function setDestino($destino){
+    $this->destino=$destino;
+}
+public function setCantMax($cant){
+    $this->canMax=$cant;
+}
+public function setCantPasajero($cant){
+    $this->cantPasajeros=$cant;
+}
+
+public function setPasajeros($pasajeros){
+    $this->objPasajeros=$pasajeros;
+}
+//-------------------------------toString
+public function __toString()
+{
+    $salida= "Codigo de viaje: ". $this->getCodigo()."\n"."Destino: ". $this->getDestino()."\n".
+    "Cantidad de pasajeros maxima: ". $this->getCantMax()."\n"."Cantidad de pasajeros: ". $this->getCantPasajeros()."\n";
+    return $salida;
+}
+}
